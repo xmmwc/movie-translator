ae9ac003311538d455fc6a5427568b333927b064
xmmwc <xmmwc@qq.com>
2019-04-04 02:13:46 +0800

    增加日志

--- src/source.js
+++ src/source.js
@@ -7,9 +7,12 @@ let token_time = null
 
 const apiGet = (param = {}) => {
   if (isTokenEx()) {
+    console.log('开始获取电影！')
     return io.get('/pubapi_v2.php', { ...param, token, app_id }).then(data => {
       if (data.error || data.error_code === 4 || !data.torrent_results) {
-        return []
+        return {
+          torrent_results: []
+        }
       }
       console.log(`成功查询到${data.torrent_results.length}跳电影记录！`)
       return data
@@ -26,7 +29,7 @@ const getToken = async () => {
   })
   token = data.token
   token_time = new Date().getTime()
-  console.log('重新获取token:', token)
+  console.log('重新获取token:', token, token_time)
   return token
 }
 
